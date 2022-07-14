f898cc05db31998967cfdbe7639c5407923bcf41
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-14 11:21:50 +0530

    sub array problem

---
+++ subArray.js
@@ -0,0 +1,9 @@
+function birthday(s, d, m) {
+  let counter = 0;
+  const reducer = (a, b) => a + b;
+  for (let i = 0; i < s.length - m + 1; i++) {
+    if (s.slice(i, i + m).reduce(reducer) == d) counter++;
+  }
+  return counter;
+}
+console.log(birthday([1, 1, 1, 1, 1], 3, 2));
