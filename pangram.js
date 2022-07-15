a9e7d805dc1fbf7356941927decc7ed9c853514e
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-15 15:08:54 +0530

    pangram

---
+++ pangram.js
@@ -0,0 +1,16 @@
+function pangrams(s) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let regex = /\s/g;
+  let lowercase = s.toLowerCase().replace(regex, "");
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (lowercase.indexOf(alphabet[i]) === -1) {
+      return "not pangram";
+    }
+  }
+
+  return "pangram";
+}
+console.log(
+  pangrams("We promptly judged antique ivory buckles for the next prize")
+);
